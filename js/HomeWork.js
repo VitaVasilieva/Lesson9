075372fd6883cce20e7fe3201d85eba60ea9bdff
VitaVasilieva <vita.sherban2016@gmail.com>
2025-02-21 21:08:42 +0200

    Add files via upload

---
+++ js/HomeWork.js
@@ -0,0 +1,24 @@
+//                   ----1----
+// const onClick = document.querySelector("#btn");
+// const text = document.querySelector("#text");
+
+// onClick.addEventListener("click", () => { text.style.display = "none";
+// });
+
+//                  ------2------
+// const onClick = document.querySelector("#btn");
+
+// onClick.addEventListener("click", () => { onClick.style.display = "none";
+// });
+
+
+//                   ------3------
+
+
+    // const tree = document.querySelector("#tree");   
+    // const toggleChildElementsButton = document.querySelector("#showTreeButton");
+    // const childElementsDiv = document.querySelector("#tree_child_elements");
+    
+    // toggleChildElementsButton.addEventListener("click", () => {
+    //     childElementsDiv.hidden = !childElementsDiv.hidden;
+    // });
